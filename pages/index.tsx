624e50715873a448ee0d8ca807c7909f752900b7
Arnon Rodrigues de Paula <arnon@MacBook-Air-de-Arnon.local>
2021-07-17 19:22:46 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,38 +13,33 @@ export default function Home() {
           <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
             <Portfolio
               image="/gallery/convite1.jpg"
-              title="Convite de Casamento"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore distinctio debitis ducimus, voluptates molestias adipisci, natus pariatur velit culpa veniam laborum incidunt quos at quo consequuntur quisquam fugit dolorem voluptatibus."
+              title="Convite de Maria"
+              description="Maria, Maria, Maria."
             />
             <Portfolio
               image="/gallery/convite1.jpg"
-              title="Convite de Casamento"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore distinctio debitis ducimus, voluptates molestias adipisci, natus pariatur velit culpa veniam laborum incidunt quos at quo consequuntur quisquam fugit dolorem voluptatibus."
+              title="Convite de José"
+              description="José, José, José."
             />
             <Portfolio
               image="/gallery/convite1.jpg"
-              title="Convite de Casamento"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore distinctio debitis ducimus, voluptates molestias adipisci, natus pariatur velit culpa veniam laborum incidunt quos at quo consequuntur quisquam fugit dolorem voluptatibus."
+              title="Convite de Antônia"
+              description="Antônia, Antônia, Antônia."
             />
             <Portfolio
               image="/gallery/convite1.jpg"
-              title="Convite de Casamento"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore distinctio debitis ducimus, voluptates molestias adipisci, natus pariatur velit culpa veniam laborum incidunt quos at quo consequuntur quisquam fugit dolorem voluptatibus."
+              title="Convite de Carol"
+              description="Carol, Carol, Carol."
             />
             <Portfolio
               image="/gallery/convite1.jpg"
-              title="Convite de Casamento"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore distinctio debitis ducimus, voluptates molestias adipisci, natus pariatur velit culpa veniam laborum incidunt quos at quo consequuntur quisquam fugit dolorem voluptatibus."
+              title="Convite de Wanderson"
+              description="Wanderson, Wanderson, Wanderson."
             />
             <Portfolio
               image="/gallery/convite1.jpg"
-              title="Convite de Casamento"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore distinctio debitis ducimus, voluptates molestias adipisci, natus pariatur velit culpa veniam laborum incidunt quos at quo consequuntur quisquam fugit dolorem voluptatibus."
-            />
-            <Portfolio
-              image="/gallery/convite1.jpg"
-              title="Convite de Casamento"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore distinctio debitis ducimus, voluptates molestias adipisci, natus pariatur velit culpa veniam laborum incidunt quos at quo consequuntur quisquam fugit dolorem voluptatibus."
+              title="Convite de Idalina"
+              description="Idalina, Idalina, Idalina."
             />
           </div>
         </section>
