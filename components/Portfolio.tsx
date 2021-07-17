c068726ec82c3930caa065486019b584bd89cb23
Arnon Rodrigues de Paula <arnon@MacBook-Air-de-Arnon.local>
2021-07-17 19:22:44 -0300

    Update Portfolio.tsx

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,11 +1,5 @@
 import Image from 'react-bootstrap/Image';
-import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon, Box
-} from "@chakra-ui/react"
+import { Accordion, AccordionItem, AccordionButton,  AccordionPanel,  AccordionIcon, Box } from "@chakra-ui/react"
 
 type PortfolioProps = {
   image: any;
@@ -16,27 +10,24 @@ type PortfolioProps = {
 export function Portfolio(props: PortfolioProps) {
   return (
     <div className="col">
-      <div className="card h-100">
+      <div className="card">
         <Image className="card-img-top" src={props.image} alt="foto do cartão" />
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
           <p className="card-text">{props.description}</p>
         </div>
-        <Accordion defaultIndex={[0]} allowMultiple>
+        <Accordion allowMultiple>
           <AccordionItem>
             <h2>
               <AccordionButton>
                 <Box flex="1" textAlign="left">
-                  Section 1 title
+                  Detalhes...
                 </Box>
                 <AccordionIcon />
               </AccordionButton>
             </h2>
             <AccordionPanel pb={4}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-              commodo consequat.
+              Aqui podemos inserir mais alguns detalhes dos convites.
             </AccordionPanel>
           </AccordionItem>
         </Accordion>
