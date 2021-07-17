55fea4ec66849971416163305b210908613d2cb7
Arnon Rodrigues de Paula <arnon@MacBook-Air-de-Arnon.local>
2021-07-17 18:53:47 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <main>
         <About />
         <section>
-          <div className="row row-cols-1 row-cols-md-3 g-4">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
             <Portfolio
               image="/gallery/convite1.jpg"
               title="Convite de Casamento"
