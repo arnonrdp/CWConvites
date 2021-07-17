c8aa7469d719400dd162db0867c659e2ce0076fa
Arnon Rodrigues de Paula <arnon@MacBook-Air-de-Arnon.local>
2021-07-17 18:53:45 -0300

    Update Portfolio.tsx

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,4 +1,11 @@
 import Image from 'react-bootstrap/Image';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon, Box
+} from "@chakra-ui/react"
 
 type PortfolioProps = {
   image: any;
@@ -15,10 +22,26 @@ export function Portfolio(props: PortfolioProps) {
           <h5 className="card-title">{props.title}</h5>
           <p className="card-text">{props.description}</p>
         </div>
-        <div className="card-footer">
-          <small className="text-muted">#categoriaA #categoriaB</small>
-        </div>
+        <Accordion defaultIndex={[0]} allowMultiple>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  Section 1 title
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
       </div>
     </div>
   )
 }
+
