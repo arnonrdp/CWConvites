9cd9174d3ae624a658b389f1daaf25822b36266b
Arnon Rodrigues <arnonrdp@gmail.com>
2022-03-14 13:51:00 -0300

    Header move to a component

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,19 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="black"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="/logo_horizontal.svg"
-          width="260"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="#"
-        text
-      >
-        <span>Casamento</span>
-      </v-btn>
-      <v-btn
-        href="#"
-        text
-      >
-        <span>Debutantes</span>
-      </v-btn>
-      <v-btn
-        href="#"
-        text
-      >
-        <span>Empresarial</span>
-      </v-btn>
-    </v-app-bar>
+    <Header />
+    <Carousel />
     <TheContent />
   </v-app>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Carousel from "./components/Carousel.vue";
 import TheContent from "./components/TheContent.vue";
 
 export default {
   name: "App",
-  components: { TheContent },
+  components: { Header, Carousel, TheContent },
   data: () => ({
     //
   }),
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar app color="black" dark>
+    <div class="d-flex align-center">
+      <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100" src="/logo_horizontal.svg" width="260" />
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn href="#" text>
+      <span>Convites</span>
+    </v-btn>
+    <v-btn href="#" text>
+      <span>Caixas</span>
+    </v-btn>
+    <v-btn href="#" text>
+      <span>Papelaria</span>
+    </v-btn>
+    <v-btn href="#" text>
+      <span>Dicas</span>
+    </v-btn>
+  </v-app-bar>
+</template>
