44fdcef423afd777aaaae843f959ebab9d61ec12
Arnon Rodrigues <arnonrdp@gmail.com>
2022-04-01 13:51:00 -0300

    Simplified Contato section

--- src/components/Contato.vue
+++ src/components/Contato.vue
@@ -1,34 +1,28 @@
 <template>
-  <v-sheet id="contact" color="#333333" dark tag="section" tile>
-    <div class="py-12"></div>
+  <v-sheet id="contact" color="#333333" class="py-16 text-center" tag="section" tile>
+    <h2 class="text-h3 font-weight-bold text-white mb-3">CONTATO</h2>
 
-    <v-container>
-      <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center">Contato</h2>
+    <v-responsive class="mx-auto mb-12" width="56">
+      <v-divider color="#9e9e9e" class="mb-1" />
+      <v-divider color="#9e9e9e" />
+    </v-responsive>
 
-      <v-responsive class="mx-auto" width="56">
-        <v-divider class="mb-1" />
-        <v-divider />
-      </v-responsive>
+    <div class="my-6">
+      <v-btn icon class="mx-2" size="x-large" color="#25d366" href="https://wa.me/559293969568" target="_blank">
+        <v-icon color="white">mdi-whatsapp</v-icon>
+      </v-btn>
 
-      <div class="text-center my-6">
-        <v-btn fab class="mx-2" large color="#25d366" href="https://wa.me/559293969568" target="_blank">
-          <v-icon>mdi-whatsapp</v-icon>
-        </v-btn>
+      <v-btn icon class="mx-2" size="x-large" color="#dd2a7b" href="https://instagram.com/cwconvites" target="_blank">
+        <v-icon color="white">mdi-instagram</v-icon>
+      </v-btn>
 
-        <v-btn fab class="mx-2" large color="#dd2a7b" href="https://instagram.com/cwconvites" target="_blank">
-          <v-icon>mdi-instagram</v-icon>
-        </v-btn>
+      <v-btn icon class="mx-2" size="x-large" color="#2196f3" href="tel:559293969568" target="_blank">
+        <v-icon color="white">mdi-phone</v-icon>
+      </v-btn>
 
-        <v-btn fab class="mx-2" large color="blue" href="tel:559293969568" target="_blank">
-          <v-icon>mdi-phone</v-icon>
-        </v-btn>
-
-        <v-btn fab class="mx-2" large color="grey" href="mailto:cwconvites@gmail.com" target="_blank">
-          <v-icon>mdi-email</v-icon>
-        </v-btn>
-      </div>
-    </v-container>
-
-    <div class="py-12"></div>
+      <v-btn icon class="mx-2" size="x-large" color="#9e9e9e" href="mailto:cwconvites@gmail.com" target="_blank">
+        <v-icon color="white">mdi-email</v-icon>
+      </v-btn>
+    </div>
   </v-sheet>
 </template>
