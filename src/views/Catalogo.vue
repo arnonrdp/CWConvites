1e696cdd31d2a7ebf9fb3dd9a3549bf95ee7e839
Arnon <arnonrdp@gmail.com>
2022-08-07 14:30:00 -0300

    Code smells

--- src/views/Catalogo.vue
+++ src/views/Catalogo.vue
@@ -4,32 +4,32 @@
     <p>Exemplo de como as imagens podem ficar direto na página ao invés de um .pdf</p>
     <br />
     <v-col>
-      <img src="https://www.w3schools.com/w3images/wedding.jpg" />
-      <img src="https://www.w3schools.com/w3images/rocks.jpg" />
-      <img src="https://www.w3schools.com/w3images/falls2.jpg" />
-      <img src="https://www.w3schools.com/w3images/paris.jpg" />
-      <img src="https://www.w3schools.com/w3images/nature.jpg" />
-      <img src="https://www.w3schools.com/w3images/mist.jpg" />
-      <img src="https://www.w3schools.com/w3images/paris.jpg" />
-      <img src="https://www.w3schools.com/w3images/underwater.jpg" />
-      <img src="https://www.w3schools.com/w3images/ocean.jpg" />
-      <img src="https://www.w3schools.com/w3images/wedding.jpg" />
-      <img src="https://www.w3schools.com/w3images/mountainskies.jpg" />
-      <img src="https://www.w3schools.com/w3images/rocks.jpg" />
-      <img src="https://www.w3schools.com/w3images/underwater.jpg" />
-      <img src="https://www.w3schools.com/w3images/wedding.jpg" />
-      <img src="https://www.w3schools.com/w3images/rocks.jpg" />
-      <img src="https://www.w3schools.com/w3images/falls2.jpg" />
-      <img src="https://www.w3schools.com/w3images/paris.jpg" />
-      <img src="https://www.w3schools.com/w3images/nature.jpg" />
-      <img src="https://www.w3schools.com/w3images/mist.jpg" />
-      <img src="https://www.w3schools.com/w3images/paris.jpg" />
-      <img src="https://www.w3schools.com/w3images/underwater.jpg" />
-      <img src="https://www.w3schools.com/w3images/ocean.jpg" />
-      <img src="https://www.w3schools.com/w3images/wedding.jpg" />
-      <img src="https://www.w3schools.com/w3images/mountainskies.jpg" />
-      <img src="https://www.w3schools.com/w3images/rocks.jpg" />
-      <img src="https://www.w3schools.com/w3images/underwater.jpg" />
+      <img src="https://www.w3schools.com/w3images/wedding.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/rocks.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/falls2.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/paris.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/nature.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/mist.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/paris.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/underwater.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/ocean.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/wedding.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/mountainskies.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/rocks.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/underwater.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/wedding.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/rocks.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/falls2.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/paris.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/nature.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/mist.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/paris.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/underwater.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/ocean.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/wedding.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/mountainskies.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/rocks.jpg" alt="sample" />
+      <img src="https://www.w3schools.com/w3images/underwater.jpg" alt="sample" />
     </v-col>
   </v-container>
 </template>
--- src/views/Pedido.vue
+++ src/views/Pedido.vue
@@ -2,19 +2,19 @@
   <v-container>
     <h1>Formulário de informações para o convite</h1>
     <v-form ref="form" v-model="valid" lazy-validation>
-      <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
-  
-      <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-  
-      <v-select v-model="select" :items="items" :rules="[(v) => !!v || 'Item is required']" label="Item" required></v-select>
-  
-      <v-checkbox v-model="checkbox" :rules="[(v) => !!v || 'You must agree to continue!']" label="Do you agree?" required></v-checkbox>
-  
-      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate"> Validate </v-btn>
-  
-      <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
-  
-      <v-btn color="warning" @click="resetValidation"> Reset Validation </v-btn>
+      <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required />
+
+      <v-text-field v-model="email" :rules="emailRules" label="E-mail" required />
+
+      <v-select v-model="select" :items="items" :rules="[(v) => !!v || 'Item is required']" label="Item" required />
+
+      <v-checkbox v-model="checkbox" :rules="[(v) => !!v || 'You must agree to continue!']" label="Do you agree?" required />
+
+      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Validate</v-btn>
+
+      <v-btn color="error" class="mr-4" @click="reset">Reset Form</v-btn>
+
+      <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>
     </v-form>
   </v-container>
 </template>
@@ -23,25 +23,25 @@
 export default {
   data: () => ({
     valid: true,
-    name: "",
-    nameRules: [(v) => !!v || "Name is required", (v) => (v && v.length <= 10) || "Name must be less than 10 characters"],
-    email: "",
-    emailRules: [(v) => !!v || "E-mail is required", (v) => /.+@.+\..+/.test(v) || "E-mail must be valid"],
+    name: '',
+    nameRules: [(v) => !!v || 'Name is required', (v) => (v && v.length <= 10) || 'Name must be less than 10 characters'],
+    email: '',
+    emailRules: [(v) => !!v || 'E-mail is required', (v) => /^\S+@\S+\.\S+$/.test(v) || 'E-mail must be valid'],
     select: null,
-    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
-    checkbox: false,
+    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
+    checkbox: false
   }),
 
   methods: {
     validate() {
-      this.$refs.form.validate();
+      this.$refs.form.validate()
     },
     reset() {
-      this.$refs.form.reset();
+      this.$refs.form.reset()
     },
     resetValidation() {
-      this.$refs.form.resetValidation();
-    },
-  },
-};
+      this.$refs.form.resetValidation()
+    }
+  }
+}
 </script>
