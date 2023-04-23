fe86c36bf10faa51e3a8044bd123d9b3f166fd08
Arnon <arnonrdp@gmail.com>
2023-04-23 20:30:01 -0300

    Rascunho da Home

--- src/views/HomeVue.vue
+++ src/views/HomeVue.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Hello Home</h1>
+  <h2>Bem-vindo à CW Convites!</h2>
+
+  <h5>
+    Somos especializados em criar cartões, convites, lembranças e muito mais, para tornar seu evento verdadeiramente único e especial. Nosso
+    portfólio apresenta uma variedade de nossos melhores trabalhos, para que você possa se inspirar e encontrar o produto perfeito para sua
+    ocasião.
+    <br />
+    Deixe-nos ajudá-lo a fazer do seu evento um sucesso. Dê uma olhada em nosso portfólio e entre em contato conosco para começarmos a
+    trabalhar juntos!
+  </h5>
 </template>
 
 <script setup></script>
