97b2e2a95c77e035c201143c04d81f8df7665c95
Arnon <arnonrdp@gmail.com>
2023-04-23 20:29:11 -0300

    Adicionado cabeçalho

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <q-layout>
-    Header
+    <TheHeader />
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
-<script setup></script>
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+</script>
+
+<style>
+body {
+  background: #800000; /* fallback for old browsers */
+  background: -webkit-linear-gradient(to bottom, #e6e7e8, #800000); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to bottom, #e6e7e8, #800000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-header class="bg-transparent" reveal>
+    <q-toolbar class="text-white">
+      <q-toolbar-title>
+        <q-img src="/logo_horizontal.svg" width="15rem" />
+      </q-toolbar-title>
+      <q-btn class="q-mr-xs" flat icon="img:/icons/whatsapp.svg" round size="lg" />
+      <q-btn class="q-mr-xs" flat icon="img:/icons/facebook.svg" round size="lg" />
+      <q-btn class="q-mr-xs" flat icon="img:/icons/twitter.svg" round size="lg" />
+      <q-btn class="q-mr-xs" flat icon="img:/icons/pinterest.svg" round size="lg" />
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script setup></script>
