27614611266b1cf35a70579f271332ce77a74f09
Arnon <arnonrdp@gmail.com>
2023-04-23 15:37:41 -0300

    Reset project

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,30 @@
-<template>
-  <v-app>
-    <Header />
-    <v-main class="pt-0">
-      <router-view />
-    </v-main>
-  </v-app>
-</template>
-
 <script setup>
-import Header from '@/components/Header.vue'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-html {
-  font-size: 16px;
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-@media screen and (max-width: 720px) {
-  html {
-    font-size: 15px;
-  }
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
-@media screen and (max-width: 640px) {
-  html {
-    font-size: 14px;
-  }
-}
-@media screen and (max-width: 560px) {
-  html {
-    font-size: 13px;
-  }
-}
-@media screen and (max-width: 480px) {
-  html {
-    font-size: 12px;
-  }
-}
-@media screen and (max-width: 360px) {
-  html {
-    font-size: 11px;
-  }
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,5 @@
-import '@quasar/extras/material-icons/material-icons.css'
-import VueSplide from '@splidejs/vue-splide'
-import { Quasar, Notify } from 'quasar'
-import 'quasar/src/css/index.sass'
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
-import router from './router'
 
-loadFonts()
-
-createApp(App)
-  .use(router)
-  .use(Quasar, {
-    plugins: { Notify }
-  })
-  .use(vuetify)
-  .use(VueSplide)
-  .mount('#app')
+createApp(App).mount('#app')
