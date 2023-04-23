8fdd2baf69a3b5206355a866ebaea57e59c53e1b
Arnon <arnonrdp@gmail.com>
2023-04-23 15:53:57 -0300

    Adicionado Quasar e Firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
+import { Quasar } from 'quasar'
 import { createApp } from 'vue'
-import './style.css'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Quasar, {
+  plugins: {} // import Quasar plugins and add here
+})
+
+// Assumes you have a <div id="app"></div> in your index.html
+app.mount('#app')
