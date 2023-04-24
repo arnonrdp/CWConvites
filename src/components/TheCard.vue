d8958fcfba3a3d2bfbda157c8f24a9e06e42f4d5
Arnon <arnonrdp@gmail.com>
2023-04-23 21:17:24 -0300

    Adicionado primeiro rascunho do card

---
+++ src/components/TheCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-card class="inline-block" style="width: 300px">
+    <img :src="img" />
+    <q-card-section>
+      <div class="text-h6">{{ label }}</div>
+      <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+defineProps({
+  img: { type: String, required: true },
+  label: { type: String, required: true }
+})
+</script>
