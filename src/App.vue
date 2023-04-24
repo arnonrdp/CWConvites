2aeb013c447a50f5301c4c2b683410b7b562ff40
Arnon <arnonrdp@gmail.com>
2023-04-23 21:17:39 -0300

    Limitação de width

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,8 @@ body {
   background: #800000; /* fallback for old browsers */
   background: -webkit-linear-gradient(to bottom, #e6e7e8, #800000); /* Chrome 10-25, Safari 5.1-6 */
   background: linear-gradient(to bottom, #e6e7e8, #800000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  margin: auto;
+  max-width: 90vw;
+  width: 1200px;
 }
 </style>
