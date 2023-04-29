b8bd7401c3d28453f933e46d8f742147d22afe6a
Arnon <arnonrdp@gmail.com>
2023-04-29 17:32:38 -0300

    Adicionado link ao logo

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,7 +2,9 @@
   <q-header class="bg-transparent" reveal>
     <q-toolbar class="text-white">
       <q-toolbar-title>
-        <q-img src="/logo_horizontal.svg" width="15rem" />
+        <RouterLink to="/">
+          <q-img src="/logo_horizontal.svg" width="15rem" />
+        </RouterLink>
       </q-toolbar-title>
       <q-btn class="q-mr-xs" flat icon="img:/icons/whatsapp.svg" round size="lg" />
       <q-btn class="q-mr-xs" flat icon="img:/icons/facebook.svg" round size="lg" />
