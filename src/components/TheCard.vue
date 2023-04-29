8cb40628077872f409a25f86ac8d78d4e00faca1
Arnon <arnonrdp@gmail.com>
2023-04-29 18:10:24 -0300

    Melhorias gerais no card de produto

--- src/components/TheCard.vue
+++ src/components/TheCard.vue
@@ -1,16 +1,21 @@
 <template>
-  <q-card class="inline-block" style="width: 300px">
-    <img :src="img" />
-    <q-card-section>
-      <div class="text-h6">{{ label }}</div>
-      <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+  <q-card class="full-height inline-block" style="width: 300px; max-width: 90vw">
+    <q-img v-if="images?.length" fit="cover" :src="images[0]" style="max-height: 10rem" />
+    <q-card-section class="flex items-center justify-between">
+      <h6 class="q-my-none">{{ name }}</h6>
+      <q-badge>{{ category.label }}</q-badge>
+    </q-card-section>
+    <q-card-section class="q-py-none" style="white-space: pre-line">
+      <p>{{ description }}</p>
     </q-card-section>
   </q-card>
 </template>
 
 <script setup>
 defineProps({
-  img: { type: String, required: true },
-  label: { type: String, required: true }
+  category: { type: Object, required: false },
+  description: { type: String, required: false },
+  images: { type: Array, required: true },
+  name: { type: String, required: true }
 })
 </script>
