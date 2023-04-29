34ce734e6b26c6b2fd5e0d23c638a24a17fd9ff2
Arnon <arnonrdp@gmail.com>
2023-04-29 08:19:12 -0300

    Adicionada store de product para adição/remoção do Storage

--- src/store/product.js
+++ src/store/product.js
@@ -1,4 +1,31 @@
-import { db } from '@/firebase'
+import { deleteObject, getDownloadURL, ref, uploadBytes } from 'firebase/storage'
 import { defineStore } from 'pinia'
+import { storage } from 'src/firebase'
 
-export const useProductStore = defineStore('products', {})
+export const useProductStore = defineStore('products', {
+  state: () => ({
+    _isLoading: false
+  }),
+
+  getters: {
+    isLoading: (state) => state._isLoading
+  },
+
+  actions: {
+    async uploadFile(file, filename) {
+      const storageRef = ref(storage, `images/${filename}`)
+
+      this._isLoading = true
+      await uploadBytes(storageRef, file).finally(() => (this._isLoading = false))
+
+      return getDownloadURL(storageRef).then((url) => url)
+    },
+
+    async deleteFile(filename) {
+      const storageRef = ref(storage, `images/${filename}`)
+
+      this._isLoading = true
+      await deleteObject(storageRef).finally(() => (this._isLoading = false))
+    }
+  }
+})
